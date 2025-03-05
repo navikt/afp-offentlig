e680cfe23324397a54276a67f2c73d6a4e0ead07
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2025-03-05 09:42:40 +0100

    Refactor ApiController.kt

--- src/main/kotlin/no/nav/pensjon/afpoffentlig/controllers/ApiController.kt
+++ src/main/kotlin/no/nav/pensjon/afpoffentlig/controllers/ApiController.kt
@@ -6,13 +6,9 @@ import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.*
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.server.ResponseStatusException
@@ -25,7 +21,7 @@ import java.util.*
 class ApiController(
     @Value("\${TP_FSS_URL}") val tpFssUrl: String
 ) {
-    private val restTemplate = createRestTemplate()
+    private val restTemplate = RestTemplateBuilder().build()
     private val logger = LoggerFactory.getLogger(javaClass)
     companion object {
         const val FNR = "fnr"
@@ -44,57 +40,33 @@ class ApiController(
         try {
             MDC.putCloseable("X-Request-Id", xRequestId).use {
                 logger.info("Received call with X-Request-Id = $xRequestId, forwarding to TP.")
-                val result = httpTemplateCallTP(fnr, xRequestId, auth)
-                result.body?.let { logger.info("Body: $it") }
-                return result.body
+                return restTempalateToTP(fnr, xRequestId, auth).body
             }
         } catch(e: HttpClientErrorException) {
-            logger.warn("Client Error with X-Request-Id = $xRequestId received error from TP: ${e.statusCode.value()}", e)
+            logger.warn("Client Error with X-Request-Id = $xRequestId and statusCode: ${e.statusCode.value()}", e)
             throw ResponseStatusException(e.statusCode, e.responseBodyAsString)
         } catch (e: HttpServerErrorException) {
-            logger.warn("Server Error from proxy with X-Request-Id = $xRequestId:  ${e.statusCode.value()}", e)
+            logger.warn("Server Error with X-Request-Id = $xRequestId, from proxy and statusCode: ${e.statusCode.value()}", e)
             throw ResponseStatusException(HttpStatus.BAD_GATEWAY)
         }
     }
 
-    private fun httpTemplateCallTP(fnr: String, xRequestId: String, auth: String): ResponseEntity<String> {
-        return restTemplate.exchange<String>(
+    private fun restTempalateToTP(fnr: String, xRequestId: String, auth: String) = restTemplate.exchange<String>(
             UriComponentsBuilder.fromUriString("$tpFssUrl/api/afpoffentlig/harAFPoffentlig").build().toString(),
             HttpMethod.GET,
             HttpEntity<Any?>(buildHeaders(fnr, xRequestId, auth))
         ).also { logger.info("statuscode: {}, body: {}", it.statusCode, it.body) }
-    }
 
-    private fun buildHeaders(fnr: String, xRequestId: String, auth: String): HttpHeaders {
-        val headers = HttpHeaders()
-        headers.add(FNR, fnr)
-        headers.add("X-Request-Id", xRequestId)
-        headers.add(HttpHeaders.AUTHORIZATION, auth)
-        return headers
-    }
+    private fun buildHeaders(fnr: String, xRequestId: String, auth: String) = HttpHeaders()
+        .apply {
+            add(FNR, fnr)
+            add("X-Request-Id", xRequestId)
+            add(HttpHeaders.AUTHORIZATION, auth)
+        }
 
     @ExceptionHandler(ResponseStatusException::class)
     fun handleResponseStatusException(e: ResponseStatusException, request: WebRequest): ResponseEntity<Any> {
         return ResponseEntity.status(e.statusCode).body(e.reason)
-    }
-
-    fun createRestTemplate(): RestTemplate {
-        return RestTemplateBuilder()
-            .additionalInterceptors(RemoveInterceptors())
-            .build()
-    }
-
-    private class RemoveInterceptors() : ClientHttpRequestInterceptor {
-        private val logger = LoggerFactory.getLogger(javaClass)
-
-        override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution
-        ): ClientHttpResponse {
-            logger.info("Removing request header ${HttpHeaders.TRANSFER_ENCODING}")
-            request.headers.remove(HttpHeaders.TRANSFER_ENCODING)
-            request.headers.remove("transfer-encoding")
-
-            return execution.execute(request, body)
-        }
     }
 
 }
